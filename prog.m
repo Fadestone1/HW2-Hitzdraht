93c1506fe153a9cd77cd3d0502aebcfb4ab81c22
Fadestone1 <41193297+Fadestone1@users.noreply.github.com>
2022-12-17 18:18:50 +0100

    Update prog.m

--- prog.m
+++ prog.m
@@ -4,11 +4,17 @@ clc;
 
 load('data.mat');
 
-figure(1)
-hold on
+
 xc = linspace(0.25,0.7,24);
-t = linspace(0,10,10000);
-for i = 1:24
-    dmp(:) = v10(1,i,:);
-    plot(t(:),dmp-v0(i,:)+2*i)
+t = linspace(0,1,10000);
+for j = 1:5
+    figure(j)
+    hold on
+    for i = 1:24
+        dmp(:) = v10(j,i,:);
+        if std(dmp-v0(i,:))>0.01
+            plot(t(:),dmp-v0(i,:)+2*i)
+        end
+    end
+    xlabel('Zeit (sec)');
 end
